d8eb665bc2f297d073e7b34c1fca97b6959d108d
An, Hyeong-woo <mytory@gmail.com>
2020-05-31 00:29:04 +0900

    updateMeta, 글 수정 권한. 추가.

--- src/MytoryNotice.php
+++ src/MytoryNotice.php
@@ -12,6 +12,8 @@ class MytoryNotice
         $this->postTypeLabel = $postTypeLabel;
         $this->postTypeKey = $postTypeKey;
         add_action( 'init', [ $this, 'registerPostType' ] );
+        add_action( 'admin_init', [ $this, 'boardCapabilities' ] );
+        add_action( "save_post_{$this->postTypeKey}", [ $this, 'updateMeta' ], 10, 3 );
     }
 
     public function registerPostType()
@@ -51,5 +53,46 @@ class MytoryNotice
         ];
 
         register_post_type( $this->postTypeKey, $args );
+    }
+
+    public function boardCapabilities() {
+        $capabilities = [
+            "edit_{$this->postTypeKey}",
+            "read_{$this->postTypeKey}",
+            "delete_{$this->postTypeKey}",
+            "delete_published_{$this->postTypeKey}" . "s", // s가 눈에 안 띌까봐 일부러 이렇게 씀.
+            "edit_{$this->postTypeKey}" . "s", // s가 눈에 안 띌까봐 일부러 이렇게 씀.
+            "edit_published_{$this->postTypeKey}" . "s", // s가 눈에 안 띌까봐 일부러 이렇게 씀.
+            "edit_other_{$this->postTypeKey}",
+            "publish_{$this->postTypeKey}",
+            "read_private_{$this->postTypeKey}",
+        ];
+
+        $roles = [ 'administrator', 'editor' ];
+
+        foreach ( $roles as $role ) {
+            $wp_role = get_role( $role );
+            foreach ( $capabilities as $capability ) {
+                $wp_role->add_cap( $capability );
+            }
+        }
+    }
+
+    /**
+     * <code><input name="meta[_mytory_board_custom_key]"></code>로 값을 넘기면 postmeta에 저장한다.
+     * <code>mytory_board</code>는 당연히 <code>$taxonomyKey</code>로 대체해야 한다.
+     *
+     * @param $post_id
+     * @param \WP_Post $post
+     * @param $is_update
+     */
+    function updateMeta( $post_id, \WP_Post $post, $is_update ) {
+        if ( ! empty( $_POST['meta'] ) ) {
+            foreach ( $_POST['meta'] as $k => $v ) {
+                if ( strpos( $k, "_{$this->taxonomyKey}_" ) === 0 ) {
+                    update_post_meta( $post_id, $k, $v );
+                }
+            }
+        }
     }
 }
